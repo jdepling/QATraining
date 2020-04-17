ddb38515aa1626bb02e79bf9517b43bf8696c06c
Justin Epling <jepling@docutechcorp.com>
2020-04-17 13:42:07 -0600

    class framework

--- UnitTestingBasics/Program.cs
+++ UnitTestingBasics/Program.cs
@@ -6,7 +6,23 @@ namespace UnitTestingBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QaClassToTest ourClass = new QaClassToTest();
+            
+            // Use our example method
+            var exampleMethodOutput = ourClass.MethodToTest(2, Item.Shotgun);
+            Console.WriteLine("ExampleMethodOutput: {0:C}", exampleMethodOutput);
+
+            // Use Adrianne's method
+            ourClass.AdrianneMethodToTest();
+
+            // Use Chris's method
+            ourClass.ChrisMethodToTest();
+
+            // Use Matt's method
+            ourClass.MattMethodToTest();
+
+            // Use Nick's method
+            ourClass.NickMethodToTest();
         }
     }
 }
