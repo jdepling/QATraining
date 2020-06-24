dedb506d94e04c2a0f4e6004ec0f6565f1659136
Justin Epling <jepling@docutechcorp.com>
2020-06-24 11:46:41 -0600

    Automating field library

--- UITestPractice2/ConformXSandBoxTest.cs
+++ UITestPractice2/ConformXSandBoxTest.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using Xunit;
 
@@ -13,10 +14,12 @@ namespace UITestPractice2
         {
             ChromeOptions chromeOptions = new ChromeOptions();
             Driver = new ChromeDriver(chromeOptions);
+            Driver.Manage().Window.Maximize();
+
+            Setup();
         }
 
-        [Fact]
-        public void TestConformX()
+        public void Setup()
         {
             Driver.Navigate().GoToUrl("https://qa.conformx.com/Login.aspx");
 
@@ -29,16 +32,66 @@ namespace UITestPractice2
             password.SendKeys("SOMEPASSWORD");
 
             Driver.FindElement(By.XPath("//*[@id=\"divUserName\"]/a")).Click();
-            Driver.FindElement(By.Id("ctl00_Header_lnkAdmin")).Click();
-            Driver.FindElement(By.LinkText("System Default Settings")).Click();//call me back, i hung up!!!
+        }
 
-            Console.WriteLine("test");
-            Driver.FindElement(By.Id("systemDefaults_defaults_btnShowHelp3")).Click();
-            Driver.FindElement(By.Id("btnClose")).Click();
-
+        public void TearDown()
+        {
             // Close the browser and release the driver so we won't have chromeDriver.exe in our task manager
             Driver.Close();
             Driver.Dispose();
+        }
+
+        public void SelectDropDownOption(string id, string option)
+        {
+            var dropdown = Driver.FindElement(By.Id(id));
+            new SelectElement(dropdown).SelectByText(option);
+        }
+
+
+        [Fact]
+        public void TestConformX()
+        {
+            try
+            {
+                Driver.FindElement(By.Id("ctl00_Header_lnkAdmin")).Click();
+                Driver.FindElement(By.LinkText("System Default Settings")).Click();
+
+                Driver.FindElement(By.Id("systemDefaults_defaults_btnShowHelp3")).Click();
+                Driver.FindElement(By.Id("btnClose")).Click();
+            }
+            finally
+            {
+                TearDown();
+            }
+        }
+
+        [Fact]
+        public void TestFieldLibrary() 
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://qa.conformx.com/admin/WebAdminFieldLibrary.aspx");
+                SelectDropDownOption("ctl00_cphBodyContent_ddCriteria", "Field Name");
+                SelectDropDownOption("ctl00_cphBodyContent_ddSearchType", "Equals");
+                Driver.FindElement(By.Id("ctl00_cphBodyContent_tbExpression")).SendKeys("Borrower 4 Credit Report Date Plus 90 Days");
+
+
+                Driver.FindElement(By.Id("ctl00_cphBodyContent_btnAddCondition")).Click();
+                Driver.FindElement(By.Id("ctl00_cphBodyContent_btnApply")).Click();
+
+                string index = Driver.FindElement(By.XPath("//*[@id=\"ctl00_cphBodyContent_datagrid\"]/tbody/tr[2]/td[1]/a")).Text;
+                Assert.Equal("141092", index);
+
+                var table = Driver.FindElement(By.XPath("//*[@id=\"ctl00_cphBodyContent_datagrid\"]/tbody/tr[2]/td[1]/a"));
+                Assert.True(table.Displayed);
+
+            }
+            finally
+            {
+                TearDown();
+            }
+        
+        
         }
     }
 }
